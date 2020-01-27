a134b7005f01ff31ead7ba15ec41286ec5057c7f
Mirjam Wehr <mirjam.wehr@googlemail.com>
2020-01-27 21:01:12 +0100

    bug fix deployment file deployment anstatt manual deployment

--- ourdeploy.sh
+++ ourdeploy.sh
@@ -2,6 +2,6 @@
 
 tag=$1
 
-sed "s/latest/$tag/" applications/hello-kenzan/k8s/manual-deployment.yaml > applications/hello-kenzan/k8s/tmp.yaml
+sed "s/latest/$tag/" applications/hello-kenzan/k8s/deployment.yaml > applications/hello-kenzan/k8s/tmp.yaml
 kubectl apply -f applications/hello-kenzan/k8s/tmp.yaml
 rm applications/hello-kenzan/k8s/tmp.yaml
